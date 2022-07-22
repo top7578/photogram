62ee3c335c09fe9cfbe32f10aff927266a3d82b0
junwoo <top7578@naver.com>
2022-07-22 17:34:07 +0900

    [FEAT] 프로필 페이지 - 프로필 페이지 본인 여부 확인하여 화면 다르게 표시

--- src/main/java/com/cos/photogramstart/service/UserService.java
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -4,6 +4,7 @@ import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.web.dto.user.UserProfileDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,12 +17,17 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User 회원프로필(int userId) {
-        User userEntity = userRepository.findById(userId)
+    public UserProfileDto 회원프로필(int pageUserId, int principalId) {
+        UserProfileDto userProfileDto = new UserProfileDto();
+
+        User userEntity = userRepository.findById(pageUserId)
                 .orElseThrow(() -> {throw new CustomException("해당 프로필 페이지는 없는 페이지입니다.");});
 
-        return userEntity;
+        userProfileDto.setUser(userEntity);
+        userProfileDto.setPageOwnerState(pageUserId == principalId);
+        userProfileDto.setImageCount(userEntity.getImages().size());
 
+        return userProfileDto;
     }
 
     @Transactional
