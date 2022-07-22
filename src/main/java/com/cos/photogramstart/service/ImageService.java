82f09b030eedde12ae4793f5e7f91d1aabb2e7bc
junwoo <top7578@naver.com>
2022-07-22 11:48:23 +0900

    [FEAT] 프로필 페이지 - Image 서버에 업로드

---
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,42 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+
+        MultipartFile file = imageUploadDto.getFile();
+
+        UUID uuid = UUID.randomUUID();
+        String imageFileName = uuid + "_" +file.getOriginalFilename();
+
+        File saveFile = new File(uploadFolder, imageFileName);
+
+        //통신, I/O -> 예외가 발생할 수 있다. 런타임 시에만 발생한다.
+        try {
+            file.transferTo(saveFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
